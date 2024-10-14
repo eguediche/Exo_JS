d9a15412d7e5d27e083a40eb6f7415e1b491b0e6
eguediche <eguediche@edenschool.fr>
2024-10-14 14:46:39 +0200

    3 exo finish
    3 exo terminer ._.

---
+++ style.js
@@ -0,0 +1,22 @@
+//exo 1
+let C = 25;
+let F = 32;
+
+F = C * 9/5 + 32;
+console.log(F);
+
+//exo 2
+
+let a = 10;
+let b = 9;
+let c = 1;
+let d = a + b + c;
+
+console.log(d);
+
+//exo 3
+
+let h = 2;
+let s = h * 3600;
+
+console.log(s);
